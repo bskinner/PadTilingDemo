3a04188f6f8ee8672d483c28a19740458455b273
Blake Skinner <bskinner@chaoticminds.org>
2015-07-29 23:02:52 -0400

    Initial commit of the base collection view cell implementation

--- TripAdvisorDemo/ImagesCollectionViewCell.swift
+++ TripAdvisorDemo/ImagesCollectionViewCell.swift
@@ -1,5 +1,31 @@
 import UIKit
 
 class ImagesCollectionViewCell: UICollectionViewCell {
+    var images: [UIImage] = [] {
+        didSet {
+            self.reloadSubviews()
+        }
+    }
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func reloadSubviews() {
+        for subview in self.subviews {
+            subview.removeFromSuperview()
+        }
+        
+        for image in self.images {
+            let imageView = UIImageView(image: image)
+            imageView.translatesAutoresizingMaskIntoConstraints = true
+            imageView.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+            imageView.frame = self.bounds
+            self.addSubview(imageView)
+        }
+    }
 }
