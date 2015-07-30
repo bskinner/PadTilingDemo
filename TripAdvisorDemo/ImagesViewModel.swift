f7cc4e663d7a8f4f377c8050803a8f2114614e11
Blake Skinner <bskinner@chaoticminds.org>
2015-07-29 23:03:14 -0400

    Initial body of the view model.
    Not sure if this is going to be needed.

--- TripAdvisorDemo/ImagesViewModel.swift
+++ TripAdvisorDemo/ImagesViewModel.swift
@@ -1,5 +1,28 @@
 import UIKit
 
-class ImagesViewModel: NSObject, UICollectionViewDataSource {
 
+class ImagesViewModel: NSObject {
+    var images: [NSIndexPath:[UIImage]] = [:]
+    
+    override init() {
+        
+    }
+    
+    subscript(indexPath: NSIndexPath) -> [UIImage]? {
+        get {
+            if let images = self.images[indexPath] {
+                return images
+            } else {
+                return nil
+            }
+        }
+        
+        set(newValue) {
+            images[indexPath] = newValue
+        }
+    }
+    
+    func numberOfImages() -> Int {
+        return self.images.count
+    }
 }
