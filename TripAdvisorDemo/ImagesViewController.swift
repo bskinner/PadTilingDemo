934dd6eba61470835cf0a9570ccdbecb71b886d4
Blake Skinner <bskinner@chaoticminds.org>
2015-07-29 23:57:47 -0400

    Updates for the changes to the view/data model

--- TripAdvisorDemo/ImagesViewController.swift
+++ TripAdvisorDemo/ImagesViewController.swift
@@ -4,7 +4,6 @@ import UIKit
 class ImagesViewController: UICollectionViewController, CollectionViewDelegateGridLayout {
     private let cellIdentifier = "ImagesCollectionViewCellIdentifier"
     private var dataSource: ImagesDataSource?
-    private var viewModel: ImagesViewModel?
     private var gridLayout: GridLayout {
         get {
             return self.collectionViewLayout as! GridLayout
@@ -23,7 +22,6 @@ class ImagesViewController: UICollectionViewController, CollectionViewDelegateGr
     override func viewDidLoad() {
         super.viewDidLoad()
         self.dataSource = ImagesDataSource()
-        self.viewModel = ImagesViewModel()
     }
     
     override func didReceiveMemoryWarning() {
@@ -36,37 +34,52 @@ class ImagesViewController: UICollectionViewController, CollectionViewDelegateGr
     }
     
     override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.viewModel!.numberOfImages()
+        guard let dataSource = self.dataSource else {
+            return 0
+        }
+        
+        return dataSource.numberOfImages()
     }
     
     override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
         return 1
     }
-override     
-    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier(cellIdentifier, forIndexPath: indexPath) as! ImagesCollectionViewCell
         
-        if let images = self.viewModel?[indexPath] {
-            cell.images = images
+        if let image = self.dataSource?[indexPath.item] {
+            cell.images = [image]
+        } else {
+            self.dataSource?.getImage(indexPath.item) {image, error in
+                if image == nil {
+                    print("failed to load image at index path \(indexPath)")
+                }
+                
+                NSOperationQueue.mainQueue().addOperationWithBlock() {
+                    collectionView.reloadItemsAtIndexPaths([indexPath])
+                }
+            }
         }
         
         return cell
     }
     
     func collectionView(collectionView: UICollectionView, heightForItemAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        guard let viewModel = self.viewModel else {
+        guard let dataSource = self.dataSource else {
             return self.gridLayout.columnWidthForCollectionView()
         }
         
-        guard let images = viewModel[indexPath] else {
+        guard let image = dataSource[indexPath.item] else {
             return self.gridLayout.columnWidthForCollectionView()
         }
+        
+        let images = [image]
         
         let maxSize = images.reduce(CGSize.zeroSize) { (maxSize, image) -> CGSize in
             return CGSize(width: max(maxSize.width, image.size.width), height: max(maxSize.height,image.size.height))
         }
         
-        return (maxSize.height / maxSize.width) * self.gridLayout.columnWidthForCollectionView()
+        return floor((maxSize.height / maxSize.width) * self.gridLayout.columnWidthForCollectionView())
     }
 }
-
